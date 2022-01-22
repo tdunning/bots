568ace016cb7fc6c7ae5bb997e536df7c3d80c32
Ted Dunning <ted.dunning@gmail.com>
2022-01-21 17:16:54 -0800

    Define some variables

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -2,11 +2,14 @@ from evdev import InputDevice, categorize, ecodes
 
 gamepad = InputDevice('/dev/input/event9')
 
+KEY_DOWN = 1
+KEY_UP = 0
+
 def start_stop(event, key, side):
     if event.type == key:
-        if event.val == DOWN:
+        if event.val == KEY_DOWN:
             print("start $side motor")
-        if event.val == UP:
+        if event.val == KEY_UP:
             print("stop $side motor")
 
 for event in gamepad.read_loop():
