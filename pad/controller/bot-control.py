8771da369dd84c40c49ed6fe2de6a941dfcb1b49
Ted Dunning <ted.dunning@gmail.com>
2022-01-21 17:12:45 -0800

    Use a function

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -2,16 +2,15 @@ from evdev import InputDevice, categorize, ecodes
 
 gamepad = InputDevice('/dev/input/event9')
 
+def start_stop(event, key, side):
+    if event.type == key:
+        if event.val == DOWN:
+            print("start $side motor")
+        if event.val == UP:
+            print("stop $side motor")
+
 for event in gamepad.read_loop():
-    if event.type == ecodes.EV_KEY:
-        if event.type == KEY_A:
-            if event.val == DOWN:
-                print("start left motor")
-            if event.val == UP:
-                print("stop left motor")
-        if event.type == KEY_B:
-            if event.val == DOWN:
-                print("start right motor")
-            if event.val == UP:
-                print("stop right motor")
+    start_stop(event, KEY_A, "left")
+    start_stop(event, KEY_B, "right")
+
 
