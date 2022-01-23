31f52f4de457035271aa254ffe267da4c3234474
Aiden2022 <98195892+Aiden2022@users.noreply.github.com>
2022-01-22 17:29:34 -0800

    Going forward

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -4,16 +4,18 @@ gamepad = InputDevice('/dev/input/event0')
 
 KEY_DOWN = 1
 KEY_UP = 0
+KEY_LB = 310
+KEY_RB = 311
 
 def start_stop(event, key, side):
-    if event.type == key:
+    if event.code == KEY_LB:
         if event.val == KEY_DOWN:
             print("start $side motor")
         if event.val == KEY_UP:
             print("stop $side motor")
 
 for event in gamepad.read_loop():
-    start_stop(event, KEY_A, "left")
-    start_stop(event, KEY_B, "right")
+    start_stop(event, KEY_LB, "left")
+    start_stop(event, KEY_RB, "right")
 
 
