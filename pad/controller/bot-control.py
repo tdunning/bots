31077f3b85aecd2771d240821332dc22a1a971b3
Aiden2022 <98195892+Aiden2022@users.noreply.github.com>
2022-01-23 18:12:35 -0800

    changwe

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -1,22 +1,23 @@
 from evdev import InputDevice, categorize, ecodes
 
 gamepad = InputDevice('/dev/input/event0')
+print('Aiden1')
 
 KEY_DOWN = 1
 KEY_UP = 0
 KEY_LB = 310
 KEY_RB = 311
-
+print('Aiden2')
 def start_stop(event, key, side):
     if event.code == KEY_LB:
         if event.val == KEY_DOWN:
             print("start $side motor")
         if event.val == KEY_UP:
             print("stop $side motor")
-
+print('Aiden3')
 for event in gamepad.read_loop():
     print('aiden:eventval', event.val , '  event code =', event.code)
     start_stop(event, KEY_LB, "left")
  #   start_stop(event, KEY_RB, "right")
-
+print('Aiden4')
 
