7e0de06405e27924b2ba1dacccdbdd3211ea151f
Aiden2022 <98195892+Aiden2022@users.noreply.github.com>
2022-02-27 10:27:29 -0800

    motor1 forward, reverse update

--- pad/controller/motors/motor1.py
+++ pad/controller/motors/motor1.py
@@ -1,27 +1,35 @@
 import RPi.GPIO as GPIO
-on = 1
+forward = 1
 off = 0
+reverse=-1
 motor1=0
 motor2=1
 in3= 34
 in4= 33
-GPIO.setup(in3, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)IO.OUT)
 GPIO.setup(in4. GPIO.OUT)
-
+GPIO.setup(in3. GPIO.OUT)
 
 def motorpower(motor, power):
     if motor == motor1: 
-        if power == on:
+        if power == forward:
             GPIO.output(in3, GPIO.HIGH)
             GPIO.output(in4, GPIO.LOW)
         if power == off:
             GPIO.output(in3, GPIO.LOW)
             GPIO.output(in4, GPIO.LOW)
-    if motor == motor2:
-        if power == on:
-            GPIO.output(in3, GPIO.HIGH)
+        if power == reverse:
+            GPIO.output(in3, GPIO.LOW)
             GPIO.output(in4, GPIO.HIGH)
+
+    if motor == motor2:
+        if power == forward:
+            GPIO.output(in3, GPIO.HIGH)
+            GPIO.output(in4, GPIO.LOW)
         if power == off:
             GPIO.output(in4, GPIO.LOW)
             GPIO.output(in3, GPIO.LOW)
+        if power == reverse:
+            GPIO.output(in3, GPIO.LOW)
+            GPIO.output(in4, GPIO.HIGH)
 
