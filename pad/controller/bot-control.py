a405b08b3a7742d7710553d9b6fb9f58b6d76b17
Aiden2022 <98195892+Aiden2022@users.noreply.github.com>
2022-03-05 15:50:11 -0800

    simplifying the code to control the motors

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -1,6 +1,5 @@
 import sys
 from motors import motor1
-from motors import motor3
 from evdev import InputDevice, categorize, ecodes
 
 gamepad = InputDevice('/dev/input' + sys.argv[1])
@@ -10,22 +9,19 @@ KEY_UP = 0
 KEY_LB = 310
 KEY_RB = 311
 KEY_A = 304
+
 def start_stop(event, key, side):
     if event.code == key:
         if event.value == KEY_DOWN:
             motor1.motorpower(side, motor1.forward)
         if event.value == KEY_UP:
             motor1.motorpower(side, motor1.off)
-    if event.code == key:
-        if key == 304:
-            if event.value == KEY_DOWN:
-                motor3.motorpower1(side, motor3.forward)
-            if event.value == KEY_UP:
-                motor3.motorpower1(side, motor3.off)
+
 for event in gamepad.read_loop():
     print(event)
     start_stop(event, KEY_LB, 0)
     start_stop(event, KEY_RB, 1)
     start_stop(event, KEY_A, 2)
+    start_stop(event, KEY_A, 3)
 print('Aiden4')
 
--- pad/controller/motors/motor1.py
+++ pad/controller/motors/motor1.py
@@ -2,40 +2,35 @@ import RPi.GPIO as GPIO
 forward = 1
 off = 0
 reverse=-1
-motor1=0
-motor2=1
-in1=35 
-in2=36
-in3= 32
-in4= 31
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(in4, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
+
+class Motor:
+    """a class to control a single motor"""
+    pin1 = -1
+    pin2 = -2
+
+    def control(self, power):
+        if power == forward:
+            GPIO.output(self.pin1, GPIO.LOW)
+            GPIO.output(self.pin2, GPIO.HIGH)
+        if power == off:
+            GPIO.output(self.pin1, GPIO.LOW)
+            GPIO.output(self.pin2, GPIO.LOW)
+        if power == reverse:
+            GPIO.output(self.pin1, GPIO.HIGH)
+            GPIO.output(self.pin2, GPIO.LOW)
+
+    def __init__(self, pin1, pin2):
+        self.pin1 = pin1
+        self.pin2 = pin2
+        GPIO.setup(self.pin1, GPIO.OUT)
+        GPIO.setup(self.pin2, GPIO.OUT)
+
+motors = [Motor(35, 36), Motor(32, 31), Motor(11, 12), Motor(15, 16)]
 
 def motorpower(motor, power):
-    if motor == motor1: 
-        if power == forward:
-            GPIO.output(in3, GPIO.LOW)
-            GPIO.output(in4, GPIO.HIGH)
-        if power == off:
-            GPIO.output(in3, GPIO.LOW)
-            GPIO.output(in4, GPIO.LOW)
-        if power == reverse:
-            GPIO.output(in3, GPIO.HIGH)
-            GPIO.output(in4, GPIO.LOW)
+    motors[motor].control(power)
 
-    if motor == motor2:
-        if power == forward:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.HIGH)
-        if power == off:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.LOW)
-        if power == reverse:
-            GPIO.output(in1, GPIO.HIGH)
-            GPIO.output(in2, GPIO.LOW)
-motorpower(0, off)
-motorpower(1, off)
+for i in range(4):
+     motorpower(i, off)   
