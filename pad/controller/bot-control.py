1f4766b2c1b3ba79825ff3d3f1abb2e64f96fea2
Aiden2022 <98195892+Aiden2022@users.noreply.github.com>
2022-03-08 16:29:06 -0800

    additional code to make the motors go reverse

--- pad/controller/bot-control.py
+++ pad/controller/bot-control.py
@@ -9,11 +9,20 @@ KEY_UP = 0
 KEY_LB = 310
 KEY_RB = 311
 KEY_A = 304
+KEY_RT = 5
+KEY_LT = 2
 
 def start_stop(event, key, side):
     if event.code == key:
         if event.value == KEY_DOWN:
             motor1.motorpower(side, motor1.forward)
+        if event.value == KEY_UP:
+            motor1.motorpower(side, motor1.off)
+
+def back_stop(event, key, side):
+    if event.code == key:
+        if event.value == KEY_DOWN:
+            motor1.motorpower(side, motor1.reverse)
         if event.value == KEY_UP:
             motor1.motorpower(side, motor1.off)
 
@@ -23,5 +32,7 @@ for event in gamepad.read_loop():
     start_stop(event, KEY_RB, 1)
     start_stop(event, KEY_A, 2)
     start_stop(event, KEY_A, 3)
+    back_stop(event, KEY_RT, 1)
+    back_stop(event, KEY_LT, 0)
 print('Aiden4')
 
